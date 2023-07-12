00579d33f83d17defe603cd286a297fc39deb093
Aliyu <biggy0023@gmail.com>
2023-07-13 00:19:47 +0100

    Separated past and upcoming events. Made it such that if not signed in, events are not click able

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,6 @@ class Event < ApplicationRecord
   belongs_to :created_by, class_name: 'User', foreign_key: 'creator_id'
   has_many :event_attendances, foreign_key: :attended_event_id
   has_many :attendees, through: :event_attendances, source: :attendee
+  scope :past, -> { where('event_date < ?', Time.now.midnight) }
+  scope :upcoming, -> { where('event_date > ?', Time.now.midnight) }
 end
